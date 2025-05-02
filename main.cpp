7963bdc40146cf0742296485a75d32925fdc8503
Gian Quispe <gian.quispe.et7@gmail.com>
2025-05-02 16:05:26 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+   cout << "Hola Gian Carlos Quispe Ñahui" << endl;
+    cout << "Curso: AyED comision k1202" << endl;
+    
+    return 0;
+}
